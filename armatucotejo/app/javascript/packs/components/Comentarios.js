ea995026ba06bc12a4bb5e357521583ef6199cf6
Gustavo Galvez <ggalvezb@unal.edu.co>
2019-03-10 11:41:25 -0500

    se agrega componente Comentarios

--- armatucotejo/app/javascript/packs/components/Comentarios.js
+++ armatucotejo/app/javascript/packs/components/Comentarios.js
@@ -8,21 +8,25 @@ import { Link } from 'react-router-dom';
 
 class Comentarios extends React.Component{
 
+
   constructor(props){
     super(props);
     this.state = {
-        username:"PepitoPrepa13",
+        username:"UsuarioNuevo1",
         name: "Pepito Preparatoria",
         age: 20,
     };
   }
+  onkeyup(){
+    alert("tecla");
+  };
 
   render(){
       const styles={
         avatar:{
-          width:"150px",
-          height:"150px",
-          borderRadius: "150px",
+          width:"120px",
+          height:"120px",
+          borderRadius: "120px",
           paddingBottom: "10px",
         },
         contenedor:{
@@ -44,27 +48,47 @@ class Comentarios extends React.Component{
             fontSize: "14px",
           }
         },
+        
 
-
+      
+        
       }
+     
 
     return(
     <div className="container secondbc">
   <div className="row">
-    <div className="col col-lg-3">
+    <div className="col-md-3">
       <img alt="Avatar" src={avatar} style={styles.avatar} />
         <h3 id="delBottom" style={styles.text.username} className="comfortaa">@{this.state.username}</h3>
 
       <div style={styles.button}>
-      <Link to="/"> <Button variant="contained" color="primary"  >Califica tu cotejo</Button></Link>
+      <Link to="/"> <Button variant="contained" color="primary">Califica tu cotejo</Button></Link>
     </div>
        
 
     </div>
-     <input type="name"   placeholder="Nombre"></input>
-     <input type="name"   placeholder="Nombre"></input>
-     <input type="name"   placeholder="Nombre"></input>
-       <textarea  rows="3" placeholder="Descripción"></textarea>
+      <div className="row">
+        <div className="bloque">
+          <h5 className="comfortaa">Tecnica</h5>
+          <input id = "juego" className="dato" type="number" placeholder="" width="50px" height="50px" min="1" max ="10"></input>
+        </div>
+        <div className="bloque">
+          <h5 className="comfortaa">Juego Limpio</h5>
+          <input className="dato" type="number" placeholder="" width="50px" height="50px" min="1" max ="10"></input>        
+        </div>
+        <div className="bloque">
+          <h5 className="comfortaa">Puntualidad</h5>
+          <input  className="dato" type="number" placeholder="" width="50px" height="50px" min="1" max ="10"></input>
+        </div>
+        <div className="bloque">
+            <textarea   rows="1" placeholder="Comentarios adicionales"></textarea>
+        </div>
+        
+       
+      </div>
+     
+      
   
 
 </div>
