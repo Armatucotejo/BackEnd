8b976eea2e10bf699daa15e4b62ad873ac73abbe
yocuervoc <yocuervoc@unal.edu.co>
2019-03-12 13:19:19 -0500

    controller

--- armatucotejo/app/controllers/application_controller.rb
+++ armatucotejo/app/controllers/application_controller.rb
@@ -1,20 +1,3 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
-     #protect_from_forgery with: :exception
 
-     #before_action :configure_permitted_parameters, if: :devise_controller?
-
-skip_before_action :verify_authenticity_token	
-
-     protected
-
-          def configure_permitted_parameters
-               devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
-
-               devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
-
-	  end
-
-=======
->>>>>>> parent of b79408a... Devise almost implemented
 end
